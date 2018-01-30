d0e80750a4d7a7e823caf6d66694805b1ca9f40a
tmrzette <35267910+tmrzette@users.noreply.github.com>
2018-01-30 17:38:21 -0600

    Add files via upload

---
+++ Shape-Up-Shape-Out/script.js
@@ -0,0 +1,75 @@
+
+let shapeContainer = document.getElementById(shapeContainer);
+let shape = document.getElementById()
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+})
+
+var shape = ;
+var shapeShown = ["rectangle", "square", "circle", "triangle"]
+
+class Shape {
+    constructor(shape) {
+        this.shape = document.getElementById;
+    }
+    showShape()
+}
+class Rectangle extends Shape {
+    constructor() {
+        super();
+
+        this.render();
+    }    
+}
+class Square extends Shape {
+    constructor() {
+        super();
+
+        this.render();
+    }    
+}
+class Circle extends Shape {
+    constructor() {
+        super();
+
+        this.render();
+    }
+}
+class Triangle extends Shape {
+    constructor() {
+        super();
+
+        this.render();
+    }
+}
+
+const makeRectangle = () => {
+
+}
+
+const makeSquare = () => {
+
+}
+
+const makeCircle = () => {
+
+}
+
+const makeTriangle = () => {
+
+}
+
+const cloneShape = () => {
+    render.this
+
+}
+*/
+var shapeContainer = document.getElementById("shapeBox");
+
+
+function myFunction() {
+    if (document.getElementById("rectangleShape").style.visibility = "hidden"
+}
+
+
